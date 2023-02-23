e16f085e65608d6ac123cb31c10187ff0886e5e3
Beatriz Ceschini <biaceschini@gmail.com>
2023-02-22 22:06:58 -0300

    add star wars no app.module

--- StarWarsApplication/src/components/star-wars/star-wars.component.ts
+++ StarWarsApplication/src/components/star-wars/star-wars.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-star-wars',
+  selector: 'app-starWars',
   templateUrl: './star-wars.component.html',
   styleUrls: ['./star-wars.component.css']
 })
