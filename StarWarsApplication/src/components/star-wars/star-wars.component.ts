dc9cd735b72f959603cbade0cd21aebe25660186
Beatriz Ceschini <biaceschini@gmail.com>
2023-02-24 22:20:45 -0300

    execução das atividades para review e correção

--- StarWarsApplication/src/components/star-wars/star-wars.component.ts
+++ StarWarsApplication/src/components/star-wars/star-wars.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CPersonagem } from 'src/app/c-personagem';
+import { PersonagensStarWarsService } from 'src/app/personagens-star-wars.service';
 
 @Component({
   selector: 'app-starWars',
   templateUrl: './star-wars.component.html',
   styleUrls: ['./star-wars.component.css']
 })
-export class StarWarsComponent {
-  public id: number = 0;
-  public nome: string='';
-  public habilidade: string = '';
-  public planeta : string = '';
-  public armas : string = '';
-  public avatar : string = '';
+export class StarWarsComponent implements OnInit {
+
+  public personagens: CPersonagem[] = [];
+
+ constructor(private _service: PersonagensStarWarsService){}
+
+ ngOnInit(): void {
+  this._service.getPersonagens().subscribe((res)=>{this.personagens = res})
+ }
+ public getPersonagensFromService(){
+
+ }
 
 
 }
